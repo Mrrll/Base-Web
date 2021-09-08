82d94e280977d515ff3912cb018eeb2b831d8f90
Mrrll <edgarmarrollo@gmail.com>
2021-09-08 17:38:59 +0200

    Personalizando navegacion

--- src/resources/Js/Components/App/NavComponent.vue
+++ src/resources/Js/Components/App/NavComponent.vue
@@ -8,7 +8,7 @@
       <v-breadcrumbs>
         <v-breadcrumbs-item href="/" class="title">
           <v-avatar>
-            <img src="favicon.png" alt="John" />
+            <img src="favicon.png" :alt="title" />
           </v-avatar>
           <v-toolbar-title class="ml-2">{{ title }}</v-toolbar-title>
         </v-breadcrumbs-item>
@@ -21,10 +21,10 @@
       <!-- //* Menu User -->
       <v-menu offset-y v-if="auth.check">
         <template v-slot:activator="{ on, attrs }">
-          <v-breadcrumbs color="primary" dark>
+          <v-breadcrumbs color="grey darken-3" dark>
             <v-breadcrumbs-item v-bind="attrs" v-on="on">
-              <v-avatar class="mr-2">
-                <img src="Img/blank_profile.png" alt="John" />
+              <v-avatar class="mr-2" size="28">
+                <img src="Img/perfil-avatar.jpg"/>
               </v-avatar>
               {{ auth.user.name }}
               <v-icon right dark>
@@ -33,13 +33,17 @@
             </v-breadcrumbs-item>
           </v-breadcrumbs>
         </template>
-        <v-list link>
-          <v-list-item-group color="primary">
+        <v-list link color="grey darken-4" dark>
+          <v-list-item-group color="grey darken-2" dark v-model="ItemSelected" >
             <v-list-item
               v-for="(item, i) in ListMenu"
               :key="i"
               :href="item.href"
+              :disabled="item.disabled"
             >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
               <v-list-item-content>
                 <v-list-item-title v-text="item.text"></v-list-item-title>
               </v-list-item-content>
@@ -64,6 +68,7 @@ export default {
   props: ['title', 'auth', 'route'],
   data() {
     return {
+      ItemSelected : '',
       NavLink: [
         {
           text: 'Home',
@@ -84,11 +89,19 @@ export default {
         },
       ],
       ListMenu: [
-        { text: 'Profile', href: '/profile' },
-        { text: 'Change Password', href: '/change' },
-        { text: 'Logout', href: '/logout' },
+        { text: 'Profile', href: '/profile', icon: 'mdi-account-cog', disabled: this.route == 'profile' ? true : false },
+        { text: 'Change Password', href: '/change', icon: 'mdi-key-variant', disabled: this.route == 'auth.password.change' ? true : false },
+        { text: 'Logout', href: '/logout', icon: 'mdi-logout-variant', disabled: this.route == 'logout' ? true : false },
       ],
     }
   },
+  mounted : function () {
+    for (let index = 0; index < this.ListMenu.length; index++) {
+      const element = this.ListMenu[index];
+      if (element.disabled) {
+        this.ItemSelected = index;
+      }
+    }
+  }
 }
 </script>
